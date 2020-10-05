083d3fd87701c361240e99e5bb54e29689132700
Arrashi <arrashilibinuko.19@ichat.sp.edu.sg>
2020-10-05 11:55:24 +0800

    first step of firebase google auth

--- crwn-clothing/src/components/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import FormInput from '../form-input/form-input.comoponent';
-import CustomButton from '../custom-button/custom-button.component'
+import CustomButton from '../custom-button/custom-button.component';
+
+import { signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -51,6 +53,7 @@ class SignIn extends React.Component {
                         required />
 
                     <CustomButton type="submit" >Sign In</CustomButton>
+                    <CustomButton onClick={signInWithGoogle} >Sign In With Google</CustomButton>
                 </form>
 
             </div>
