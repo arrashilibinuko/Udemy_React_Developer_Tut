e5720fa7c4ebb9da6a590957e50bed4ecd5ffb95
Arrashi <arrashilibinuko.19@ichat.sp.edu.sg>
2020-10-07 17:41:04 +0800

    collection page

--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
-import store from './redux/store';
+import { PersistGate } from "redux-persist/integration/react";
+
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
-  <Provider store={store} >
+  <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
